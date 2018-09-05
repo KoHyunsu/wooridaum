7948ed16ee533b048c6dee5f3b0928135ca296bf
KoHyunsu <sce06147@naver.com>
2018-09-06 03:22:18 +0900

    Revert "[Edit] Add New Page"
    This reverts commit def0cc4124ff389f8852c50a0f8c3e3c3bf9e32a.

--- wooridaum/src/app/index.module.js
+++
@@ -1,4 +0,0 @@
-(function () {
-  angular
-    .module('pms', ['ngSha', 'ngCookies', 'ngSanitize', 'ui.router', 'toastr', 'ngMaterial', 'ngMdIcons', 'md.data.table', 'ngFileUpload', 'ngSessionStorage', 'ckeditor']);
-}());
