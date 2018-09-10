cb41a31663c6d325b988f612048af6c104627e08
Jokuna <els9911@gmail.com>
2018-09-10 20:34:48 +0900

    [Edit] address about login, register and forgot-password.html

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index.html');
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function(req, res) {
   res.render('login.html');
+});
+
+router.get('/register', function(req, res) {
+  res.render('register.html');
+});
+
+router.get('/forgot', function(req, res) {
+  res.render('forgot-password.html');
 });
 
 
